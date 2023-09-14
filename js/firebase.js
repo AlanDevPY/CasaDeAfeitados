953fa7f7f9d8c0c7397447a4da86e2cc9c9420e5
Emanuromet <emanuromet@gmail.com>
2023-09-13 20:42:35 -0400

    add code

---
+++ js/firebase.js
@@ -0,0 +1,48 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCl5-wKr_UEQ_DmpnAF66QQMNng2ytKdZ4",
+  authDomain: "casadeafeitados-46fd3.firebaseapp.com",
+  projectId: "casadeafeitados-46fd3",
+  storageBucket: "casadeafeitados-46fd3.appspot.com",
+  messagingSenderId: "174258593812",
+  appId: "1:174258593812:web:000dafc793595b4e08c701"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+
+//   Funcion para agregar clientes a la base de datos de firestore
+export const addclientes = (nombre, apellido, telefono) => {
+  try {
+    addDoc(collection(db, "clientes"), {
+      nombre,
+      apellido,
+      telefono,
+    });
+  } catch (error) {
+    console.log("No fue posible agregar los datos" + error);
+  }
+}
+export const addAgendamiento = (nombre, apellido, telefono, hora, servicios) => {
+  try {
+    addDoc(collection(db, "Turnos"), {
+      nombre,
+      apellido,
+      telefono,
+      hora,
+      servicios
+    });
+  } catch (error) {
+    console.log("No fue posible agregar los datos" + error);
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,38 @@
+import { addclientes, addAgendamiento } from "/js/firebase.js";
+
+let btnAgendar = document.getElementById("btnAgendar");
+
+btnAgendar.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    
+    let nombre = document.getElementById("inputNombre").value;
+    let apellido = document.getElementById("inputApellido").value;
+    let telefono = document.getElementById("inputTelefono").value;
+    let hora = document.getElementById("inputHora").value;
+    let servicios = []
+    
+    // Obtén una lista de todos los elementos checkbox
+    const checkboxes = document.querySelectorAll('.form-check-input');
+    
+    // Recorre todos los elementos checkbox
+    checkboxes.forEach(checkbox => {
+        // Verifica si el checkbox está marcado
+        if (checkbox.checked) {
+            servicios.unshift(checkbox.id)
+            
+        } else {
+            // El checkbox no está marcado
+        }
+    });
+
+
+    addclientes(nombre, apellido, telefono);
+    addAgendamiento(nombre, apellido, telefono, hora,servicios);
+
+
+
+
+
+
+});
