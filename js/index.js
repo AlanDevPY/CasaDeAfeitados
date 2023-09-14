423e247da84a5d01978cffaf71020354c5659f13
Emanuromet <emanuromet@gmail.com>
2023-09-13 21:07:45 -0400

    add code

--- js/index.js
+++ js/index.js
@@ -5,30 +5,42 @@ let btnAgendar = document.getElementById("btnAgendar");
 btnAgendar.addEventListener("click", (e) => {
     e.preventDefault()
 
-
+    
     let nombre = document.getElementById("inputNombre").value;
     let apellido = document.getElementById("inputApellido").value;
     let telefono = document.getElementById("inputTelefono").value;
     let hora = document.getElementById("inputHora").value;
     let servicios = []
-
+    
     // Obtén una lista de todos los elementos checkbox
     const checkboxes = document.querySelectorAll('.form-check-input');
-
+    
     // Recorre todos los elementos c    heckbox
     checkboxes.forEach(checkbox => {
         // Verifica si el checkbox está marcado
         if (checkbox.checked) {
             servicios.unshift(checkbox.id)
-
+            
         } else {
             // El checkbox no está marcado
         }
     });
-    addAgendamiento(nombre, apellido, telefono, hora, servicios);
-    addclientes(nombre, apellido, telefono);
+
+    if(!(nombre === "" || telefono === "" || apellido === "")){
+        addclientes(nombre, apellido, telefono);
+        addAgendamiento(nombre, apellido, telefono, hora,servicios);
+        console.log("Datos Agregados");
+        document.getElementById("inputNombre").value = "";
+        document.getElementById("inputApellido").value = "";
+        document.getElementById("inputTelefono").value = "";
+
+    }else{
+        console.log("No puede agregar datos");
+    }
+    
 
 
 
+    
 
 });
