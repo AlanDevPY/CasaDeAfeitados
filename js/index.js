066135ffd22cb8869c4335e6a2d4396bad95db18
Emanuromet <emanuromet@gmail.com>
2023-09-13 20:51:24 -0400

    add code

--- js/index.js
+++ js/index.js
@@ -5,32 +5,28 @@ let btnAgendar = document.getElementById("btnAgendar");
 btnAgendar.addEventListener("click", (e) => {
     e.preventDefault()
 
-    
+
     let nombre = document.getElementById("inputNombre").value;
     let apellido = document.getElementById("inputApellido").value;
     let telefono = document.getElementById("inputTelefono").value;
     let hora = document.getElementById("inputHora").value;
     let servicios = []
-    
+
     // Obtén una lista de todos los elementos checkbox
     const checkboxes = document.querySelectorAll('.form-check-input');
-    
-    // Recorre todos los elementos checkbox
+
+    // Recorre todos los elementos c    heckbox
     checkboxes.forEach(checkbox => {
         // Verifica si el checkbox está marcado
         if (checkbox.checked) {
             servicios.unshift(checkbox.id)
-            
+
         } else {
             // El checkbox no está marcado
         }
     });
-
-
+    addAgendamiento(nombre, apellido, telefono, hora, servicios);
     addclientes(nombre, apellido, telefono);
-    addAgendamiento(nombre, apellido, telefono, hora,servicios);
-
-
 
 
 
